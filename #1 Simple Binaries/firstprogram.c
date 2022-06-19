85fc73e68cc89e3c945a657fae1e8dda00e6cf69
Faizan Nehal <45764507+FaizanNehal@users.noreply.github.com>
2022-06-19 19:21:54 +0500

    Add files via upload

---
+++ #1 Simple Binaries/firstprogram.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int i=0;
+    for(i;i<=5;i++)
+    {
+        if(i%2==0){
+            puts("Happy ");
+        }
+        else{
+            puts("Hacking!\n");
+        }
+    }
+    return 0;
+}
